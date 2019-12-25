ec0d2b522d7a4f4efe716d0f77a95c3a10e51021
LegBone <cowby123@gmail.com>
2019-12-25 21:51:07 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+cd ~
+wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+tar -zxvf go1.13.5.linux-amd64.tar.gz
+rm -rf go1.13.5.linux-amd64.tar.gz
+mkdir gopath
+cd gopath
+mkdir src bin pkg
+cd ~
+echo "export GOPATH=$HOME/gopath" >> .bashrc
+echo "export PATH=$PATH:$HOME/go/bin" >> .bashrc
+sudo source ~/.bashrc
