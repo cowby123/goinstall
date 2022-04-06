83b3e3fad7a82ab3c784fe5ea6d6625b3fd13d07
LegBone <cowby123@gmail.com>
2022-04-06 12:16:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 cd ~
-wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
-tar -zxvf go1.15.linux-amd64.tar.gz
-rm -rf go1.15.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.18.linux-amd64.tar.gz
+tar -zxvf go1.18.linux-amd64.tar.gz
+rm -rf go1.18.linux-amd64.tar.gz
 mkdir gopath
 cd gopath
 mkdir src bin pkg
