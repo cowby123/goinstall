9c7495876ae297fba87938b8758258098a948fdf
LegBone <cowby123@gmail.com>
2025-06-09 07:51:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 cd ~
-wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
-tar -zxvf go1.19.linux-amd64.tar.gz
-rm -rf go1.19.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz
+tar -zxvf go1.24.4.linux-amd64.tar.gz
+rm -rf go1.24.4.linux-amd64.tar.gz
 mkdir gopath
 cd gopath
 mkdir src bin pkg
